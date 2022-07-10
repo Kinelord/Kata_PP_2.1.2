062c5cce6ad33fcb96abd014881246a502bdb13c
Igor Shakirov <igorshakirov01101996@gmail.com>
2022-07-10 17:06:32 +0300

    Kata_PP_2.1.2_V_1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean1 == bean);
+        System.out.println(catBean1 == catBean);
     }
 }
